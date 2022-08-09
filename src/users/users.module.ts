1898f6de2ca1dfe847a806854538e0bf725cb410
gabriel griffin <jose.griffin@uraccan.edu.ni>
2022-08-09 10:05:16 -0600

    Entity users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customer.entity';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 
 import { ProductsModule } from '../products/products.module';
+import { Order } from './entities/order.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([Customer, Order])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
